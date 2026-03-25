f8a00f80378ab7a66de287eb24845d1bb655e302
Marskowych <vladiyan20@gmail.com>
2026-03-25 20:45:57 +0200

    додав ініціалізацію датабази startup.db у VerBoss-1/data також добавив до docker-compose.yml +aiosqlite, змінив у requirements.txt aiosqlite щоб була до останньоїверсії адже не виходить зі старою. Також додав до DockerFile RUN pip list щоб дебажити всі ліби, які запускаються у докері. Протестував на докері, все працює.

--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import declarative_base
+
 # Отримуємо шлях до бази з перемінних оточення Docker (або ставимо дефолт)
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:////app/data/startup.db")
-
+engine = create_async_engine(DATABASE_URL)
+Base = declarative_base()
 app = FastAPI(title="Plugin Translation Startup")
 
 app.add_middleware(
@@ -22,6 +26,11 @@ async def root():
         "database_connected": DATABASE_URL.split("/")[-1], # Покаже назву файлу бази
         "test-1": "just a code for a test-1 branch",
     }
+# Створює датабазу при запуску бекенду (якщо немає)
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 # ТУТ УЧНІ БУДУТЬ ДОДАВАТИ СВІЙ КОД:
 # 1. Створення моделей (SQLAlchemy)
