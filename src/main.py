126c1293a5c3d1a20edc8f3cea2a0a4137d4c6c5
Marskowych <vladiyan20@gmail.com>
2026-04-08 20:23:42 +0300

    Nginx work

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
-
+from contextlib import asynccontextmanager
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
@@ -12,7 +12,7 @@ from database.database import Base
 from schemas.user import UserAddSchema
 # Отримуємо шлях до бази з перемінних оточення Docker (або ставимо дефолт)
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:////app/data/startup.db")
-FRONTEND_URL = os.getenv("FRONTEND_URL")
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 engine = create_async_engine(DATABASE_URL)
 app = FastAPI(title="Plugin Translation Startup")
 # Створення сесії та генератору
@@ -25,7 +25,7 @@ SessionDep = Annotated[AsyncSession, Depends(get_session)]
 # Корс для підключення бекенду. (FRONTEND_URL береться з docker-compose.yml)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["FRONTEND_URL"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -38,24 +38,42 @@ async def root():
         "database_connected": DATABASE_URL.split("/")[-1], # Покаже назву файлу бази
         "test-1": "just a code for a test-1 branch",
     }
+
+
 # Створює датабазу при запуску бекенду (якщо немає)
-@app.post("/create_data")
-async def create_data():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    return {"Data" : "Created"}
+
+    yield
+
+    await engine.dispose()
 
 # ТУТ УЧНІ БУДУТЬ ДОДАВАТИ СВІЙ КОД:
 # 1. Створення моделей (SQLAlchemy)
 # 2. Створення схем (Pydantic)
 # 3. Ендпоінти (app.post("/translate") тощо)
+
+
 # 3.1 Отримання всіх юзерів
 @app.get("/get_all_users")
 async def get_all_users(session: SessionDep):
     query = select(user)
     res = await session.execute(query)
     return res.scalars().all()
+
+
 # 3.2 Додавання нового юзера
 @app.post("/add_new_user")
 async def add_new_user(user: UserAddSchema, session: SessionDep):
     return {"Endpoint" : "Is working"}
+
+# 3.3 тестовий ендпоінт щоб перевірити айпі користувача
+@app.get("/get_ip")
+async def get_user_ip(request: Request):
+        return {
+        "client": request.client.host,
+        "headers": dict(request.headers),
+        "Base" : list(Base.metadata.tables.keys())
+    }
