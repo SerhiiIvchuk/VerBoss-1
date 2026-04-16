24201c3ba8f16a5327767bcdc3b386a3ffca3ea0
dmitro <dmitrop369@.com>
2026-04-16 15:39:31 +0300

    extension-ui added

---
+++ extension-ui/src/components/TestModul.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h3 style="color: green;">{{ message }}</h3>
+        <p>Лічильник: {{ count }}</p>
+        <button @click="increment">Додати</button>
+    </div>
+</template>
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+    name: "TestModuleOne",
+
+    // setup() залишається таким же, як ти звик
+    setup() {
+        const message = "I am text of the test component one";
+        const count = ref(0);
+
+        const increment = () => {
+            count.value++;
+        };
+
+        // Ось твій mounted (життєвий цикл)
+        onMounted(() => {
+            console.log("Компонент один завантажився!");
+        });
+
+        // Все, що хочемо бачити в шаблоні, повертаємо тут
+        return {
+            message,
+            count,
+            increment,
+        };
+    }
+}
+</script>
+
+<style scoped>
+/* Стилі можна винести сюди, щоб не захаращувати HTML */
+h3 {
+    font-family: sans-serif;
+}
+</style>
---
+++ extension-ui/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+       //! Вказуємо Vite, що ми не хочемо складних назв файлів з хешами
+    rolldownOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+})
