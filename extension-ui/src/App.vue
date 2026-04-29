fa48e828a2bc159cb972a75eaba20a63522a5e99
dmitro <dmitrop369@.com>
2026-04-29 09:53:16 +0300

    front auth

--- extension-ui/src/App.vue
+++ extension-ui/src/App.vue
@@ -1,19 +1,21 @@
 <script setup>
-import TestModuleOne from './components/TestModul.vue';
+// import TestModuleOne from './components/TestModul.vue';
 // import HelloWorld from './components/HelloWorld.vue'
+import VerBoss from './components/VerBoss.vue';
 </script>
 
 <template>
   <!-- <HelloWorld /> -->
-  <h1>I am VerBoss on Vue</h1>
-  <TestModuleOne />
+  <!-- <TestModuleOne /> -->
+  <h1>I am VerBoss on Vue 28</h1>
+  <VerBoss />
 </template>
 
 
 <!--! npm run build -- --watch 
 C:\Users\User>cd GitClone
-
+chrome-extension://gkfpkmhlbplbjkibndejnjnolhgb/index.html
 C:\Users\User\GitClone>cd VerBoss-1
-
+  cd GitClone\VerBoss-1\extension-ui
 C:\Users\User\GitClone\VerBoss-1>cd extension-ui
 -->
