21da53d03334dc219953dc9786ee377924a87e21
Marskowych <vladiyan20@gmail.com>
2026-05-02 16:37:13 +0300

    fix stt

--- src/ai/stt.py
+++ src/ai/stt.py
@@ -1,25 +1,45 @@
+from concurrent.futures import ThreadPoolExecutor
 import whisper
 import tempfile
+import asyncio
 import os
 from fastapi import APIRouter, UploadFile, File
 
 model = whisper.load_model("medium")
 
+executor = ThreadPoolExecutor(max_workers=3)
+
 router = APIRouter()
+
+def transcribe_sync(path: str):
+    return model.transcribe(
+        path,
+        language=None,
+        condition_on_previous_text=True
+    )
+
+async def transcribe_async(path: str):
+    loop = asyncio.get_event_loop()
+
+    return await loop.run_in_executor(
+        executor,
+        lambda: transcribe_sync(path)
+    )
 
 @router.post("/transcribe")
 async def stt(file: UploadFile = File(...)):
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
-        tmp.write(await file.read())
         path = tmp.name
-
-    result = model.transcribe(path, language="uk", task="transcribe")
-
-    os.remove(path)
-
-    return {
-        "text": result["text"],
-        "segments": result.get("segments", []),
-        "language": result.get("language")
-    }
+        try:
+            while chunk := await file.read(1024 * 1024):
+                tmp.write(chunk)
+            
+            tmp.close()
+        
+            result = await transcribe_async(path)
+            
+            return {"result": result}
+        finally:
+            os.remove(path)
+            print(type(result))
